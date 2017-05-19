fcbcccc1f0b9015b214a288670be775b9de206f4
Nguyen Thang <thangn.1411@gmail.com>
2017-05-19 20:11:04 +0700

    Update pix.go

--- pix.go
+++ pix.go
@@ -1,5 +1,18 @@
 package pix
 
+import (
+  "github.com/thang14/pix/router"
+  "github.com/thang14/pix/middleware
+)
 
+type Config struct {
+  logging bool
+}
+
+// Server contains instance details for the server
 type Server struct {
+  cfg *Config
+  routers []Router
+  middlewares []middleware.Middleware
+  servers []*HTTPServer
 }
